aaac906656ce867f38389d8a46d1efddae18f84a
MD Abdul Ahad <iamahad1570@gmail.com>
2023-10-11 14:40:41 +0600

    profile system add

--- app_login/urls.py
+++ app_login/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from app_login import views
+
 app_name = 'App_Login'
 
 
 
 urlpatterns = [
-    path('signup/', views.signup, name="signup")
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('change-profile/', views.user_change, name='user_change'),
+    path('password/', views.pass_change, name='password'),
 ]
--- app_login/views.py
+++ app_login/views.py
@@ -1,15 +1,72 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from app_login.forms import SignUpForm, UserProfileChange
 
 # Create your views here.
 def signup(request):
-    form = UserCreationForm()
+    form = SignUpForm()
     registered = False
     if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
+        form = SignUpForm(data=request.POST)
         if form.is_valid():
             form.save()
             registered = True
 
     dict = {'form': form, 'registered':registered}
     return render(request, 'app_login/signup.html', context=dict)
+
+
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+    return render (request, 'app_login/login.html', context={'form': form})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+
+@login_required
+def profile(request):
+    return render(request, 'App_Login/profile.html', context={})
+
+
+@login_required
+def user_change(request):
+    current_user = request.user
+    changed = False
+    form = UserProfileChange(instance=current_user)
+    if request.method == 'POST':
+        form = UserProfileChange(request.POST, instance=current_user)
+        if form.is_valid():
+            form.save()
+            form = UserProfileChange(instance=current_user)
+            changed = True
+    return render(request, 'app_login/change_profile.html', context={'form':form, 'changed': changed})
+
+
+@login_required
+def pass_change(request):
+    current_user = request.user
+    changed = False
+    form = PasswordChangeForm(current_user)
+    if request.method == 'POST':
+        form = PasswordChangeForm(current_user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            changed = True
+    return render (request, 'app_login/change_pass.html', context={'form':form, 'changed':changed})
